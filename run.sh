42ec05cd08d1b94cada4b841e82c12b15de1ce4c
Jordan Walke <jordojw@gmail.com>
2016-09-09 14:21:51 -0700

    add comment in run script

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-./js-give-me-a-reason.js "$1"|../Reason/refmt_impl.native -parse json -print re -use-stdin true -is-interface-pp false
+./js-give-me-a-reason.js "$1"|../Reason/refmt_impl.native -parse json -print re -use-stdin true -is-interface-pp false -assume-explicit-arity true
+
+
+# How to test the structure of various Reason syntax:
+# echo "(1, 2)" | ../Reason/refmt_impl.native -parse re -print json -is-interface-pp false -use-stdin true
+#
+# echo "ee ? ifTrue : ifFalse " | ../Reason/refmt_impl.native -parse re -print json -is-interface-pp false -use-stdin true
+
